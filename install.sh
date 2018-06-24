e014117a6632b076281c56d81ab40a1ed855b336
I. Vidal <irina.vidal+github@gmail.com>
2018-06-25 01:51:17 +0200

    some deps

---
+++ install.sh
@@ -0,0 +1,4 @@
+pip install -r requirements.txt
+pip install ~/Downloads/torch-0.3.0.post4-cp35-cp35m-linux_x86_64.whl
+pip install torchvision
+pip install git+https://github.com/szagoruyko/pyinn.git@master
