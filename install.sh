dae19341d86ab799ebc2689b9b174110eaaa542c
I. Vidal <irina.vidal+github@gmail.com>
2018-08-17 02:47:35 +0200

    remove obsolete scripts

--- install.sh
+++
@@ -1,5 +0,0 @@
-pip install -r requirements.txt
-pip install http://download.pytorch.org/whl/cu90/torch-0.3.1-cp36-cp36m-linux_x86_64.whl
-#pip install ~/Downloads/torch-0.3.0.post4-cp35-cp35m-linux_x86_64.whl
-pip install torchvision
-pip install git+https://github.com/szagoruyko/pyinn.git@master
